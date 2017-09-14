98aa343c439a5a3e3129285e684bf03aa3415265
Aurélien Delogu <dev@dreamysource.fr>
2017-09-14 14:16:36 +0200

    Add challenger

---
+++ src/Challenger.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace StreetFight;
+
+use Closure;
+use Psr\Container\ContainerInterface;
+
+/**
+ * A challenger
+ */
+class Challenger implements ChallengerInterface
+{
+    /**
+     * Challenger's name
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * The callback to run
+     *
+     * @var Closure
+     */
+    private $callback;
+
+    /**
+     * Constructor
+     *
+     * @param string $name
+     * @param Closure $callback
+     */
+    public function __construct(string $name, Closure $callback)
+    {
+        $this->name = $name;
+        $this->callback = $callback;
+    }
+
+    /**
+     * Return the challenger's name
+     *
+     * @return string
+     */
+    public function sayMyName() : string
+    {
+        return $this->name;
+    }
+
+    /**
+     * Run the callback
+     *
+     * @param Psr\Container\ContainerInterface $container
+     * @return void
+     */
+    public function kick(ContainerInterface $container) : void
+    {
+        ($this->callback)($container);
+    }
+}
---
+++ src/ChallengerInterface.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Challenger interface
+ */
+interface ChallengerInterface
+{
+    /**
+     * Return the challenger's name
+     *
+     * @return string
+     */
+    public function sayMyName() : string;
+
+    /**
+     * Run the callback
+     *
+     * @param Psr\Container\ContainerInterface $container
+     * @return void
+     */
+    public function kick(ContainerInterface $container) : void;
+}
